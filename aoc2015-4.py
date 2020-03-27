9f77ef2a7d007117510b53172f837d013107a644
annaoskarson <58904189+annaoskarson@users.noreply.github.com>
2020-03-27 22:06:34 +0100

    Update aoc2015-4.py
    Hade visst söpit bort lösningen. Återställt.

--- aoc2015-4.py
+++ aoc2015-4.py
@@ -1,16 +1,16 @@
 import hashlib
 
 data = 'yzbqklnj'
-
 i = 1
+
 
 while True:
     num = str(i)
     string = data+num
     test = hashlib.md5(string.encode())
-#    h = hashlib.new(data+num)
-#    test = h.hexdigest()
-#    test = hash.hexdigest(data + num)
-    print(string)
-    print(test)
+    result = test.hexdigest()
+    if result[:5] == '00000':
+        print('Answer A:', num)
+    if result[:6] == '000000':
+        print('Answer B:', num)
     i += 1
