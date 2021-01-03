f9a99fbc8b68ee3fd47c7b87ed447315a0645872
annaoskarson <58904189+annaoskarson@users.noreply.github.com>
2021-01-03 22:26:43 +0100

    Update aoc2015-16.py

--- aoc2015-16.py
+++ aoc2015-16.py
@@ -1,61 +1,44 @@
 with open('aoc2015-16-input.txt') as file:
-    intext = file.readlines()
+    intext = file.read().strip().split('\n')
 
-intext = [line.strip() for line in intext]
+#print(intext)
 
 sues = {}
 for text in intext:
-    type(text)
-#    print(text)
-    text = text.split(':',1)
-    sue = text[0]
-    text = text[1]
-    text2 = text.split(',')
+    sue = text.split(':',1)[0]
+    stuff = text.split(' ',2)[2].split(',')
     things = []
-    for a in text2:
+    sues[sue] = {}
+    for a in stuff:
         (item, number) = a.strip().split(':')
-        things += [(item, int(number))]
-    sues[sue] = things
+        sues[sue][item] = int(number)
 
 def partone():
-    def checksue(aunt):
-        checklist = [('children', 3), ('cats', 7), ('samoyeds', 2), ('pomeranians', 3), ('akitas', 0), ('vizslas', 0), ('goldfish', 5), ('trees', 3), ('cars', 2), ('perfumes', 1)]
-        results = []
-        for check in checklist:
-            if [item for item in sues[aunt] if item == check] or not [item for item in sues[aunt] if item[0] == check[0]]:
-                pass
-            else:
-                return(False)
-        return(True)
 
-    for sue in sues:
-        if checksue(sue):
-            print('Part one: The right aunt is', sue)
+    def checksue(aunt):
+        checklist = {'children': 3, 'cats': 7, 'samoyeds': 2, 'pomeranians': 3, 'akitas': 0, 'vizslas': 0, 'goldfish': 5, 'trees': 3, 'cars': 2, 'perfumes': 1}
+        return(all([checklist[item] == sues[aunt][item] for item in sues[aunt].keys()]))
+
+    return(next(sue for sue in sues.keys() if checksue(sue)))
 
 def parttwo():
+
     def checksue(aunt):
-        def checkitem(machine, my):
-            if machine[0] == my[0]:
-                if machine[0] in ['cats', 'trees']:
-                    return(my[1] > machine[1])
-                elif machine[0] in ['pomeranians', 'goldfish']:
-                    return(my[1] < machine[1])
-                else:
-                    return(my[1] == machine[1])
 
-        checklist = [('children', 3), ('cats', 7), ('samoyeds', 2), ('pomeranians', 3), ('akitas', 0), ('vizslas', 0), ('goldfish', 5), ('trees', 3), ('cars', 2), ('perfumes', 1)]
-        result = []
-        for check in checklist:
-            if [item for item in sues[aunt] if item[0] == check[0]]:
-                [aitem] = [item for item in sues[aunt] if item[0] == check[0]]
-                result = result + [checkitem(check, aitem)]
+        def checkitem(item):
+            if item in ['cats', 'trees']:
+                return(checklist[item] < sues[aunt][item])
+            elif item in ['pomeranians', 'goldfish']:
+                return(checklist[item] > sues[aunt][item])
             else:
-                result = result + [True]
-        return(all(result))
+                return(checklist[item] == sues[aunt][item])
 
-    for sue in sues:
-        if checksue(sue):
-            print('Part two: The right aunt is', sue)
+        checklist = {'children': 3, 'cats': 7, 'samoyeds': 2, 'pomeranians': 3, 'akitas': 0, 'vizslas': 0, 'goldfish': 5, 'trees': 3, 'cars': 2, 'perfumes': 1}
+        return(all([checkitem(thing) for thing in sues[aunt].keys()]))
 
-partone()
-parttwo()
+    return(next(sue for sue in sues.keys() if checksue(sue)))
+
+print('Advent of Code 2015, day 16 part 1')
+print(partone())
+print('Advent of Code 2015, day 16 part 2')
+print(parttwo())
